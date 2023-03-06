9fd09da7bd12987b927f1f9b3ed2f48c4f381379
juhotuominen <juhonen00@gmail.com>
2023-03-06 12:22:14 +0200

    add % ^ v  -operators and prompt mode, when program is executed with "-p"

--- Source.cpp
+++ Source.cpp
@@ -28,90 +28,136 @@ int postfix_calc(string expr) {
             
             switch (expr[i]) {
             // a operator outputs an average of inputted numbers. 
-            case 'a': {
-                int sum = 0;
-                int stSize = st.size();
-                while (!st.empty()) {
-                    int num = st.top();
-                    sum += num;
-                    st.pop();
+                case 'a': {
+                    int sum = 0;
+                    int stSize = st.size();
+                    while (!st.empty()) {
+                        int num = st.top();
+                        sum += num;
+                        st.pop();
+                    }
+                    sum /= stSize;
+                    st.push(sum);
+                    break;
                 }
-                sum /= stSize;
-                st.push(sum);
-                break;
-            }
 
-            // s operator sums up input. For example "1 2 3 s" outputs "6"
-            case 's': {
-                int sum = 0;
-                while (!st.empty()) {
-                    int num = st.top();
-                    sum += num;
-                    st.pop();
+                // s operator sums up input. For example "1 2 3 s" outputs "6"
+                case 's': {
+                    int sum = 0;
+                    while (!st.empty()) {
+                        int num = st.top();
+                        sum += num;
+                        st.pop();
+                    }
+                    st.push(sum);
+                    break;
                 }
-                st.push(sum);
-                break;
-            }
 
-            case '+': {
-                int operand2 = st.top();
-                st.pop();
-                int operand1 = st.top();
-                st.pop();
-                st.push(operand1 + operand2);
-                break;
-            }
-            case '-': {
-                int operand2 = st.top();
-                st.pop();
-                int operand1 = st.top();
-                st.pop();
-                st.push(operand1 - operand2);
-                break;
-            }
-            case '*': {
-                int operand2 = st.top();
-                st.pop();
-                int operand1 = st.top();
-                st.pop();
-                st.push(operand1 * operand2);
-                break;
-            }
-            case '/': {
-                int operand2 = st.top();
-                st.pop();
-                int operand1 = st.top();
-                st.pop();
-                st.push(operand1 / operand2);
-                break;
-            }
-             /* If there is "x" in between numbers and operator,
-             switch numbers position in stack */
-            case 'x': {
-                int operand2 = st.top();
-                st.pop();
-                int operand1 = st.top();
-                st.pop();
-                st.push(operand2);
-                st.push(operand1);
-                break;
-            }
-            default: {
-                cerr << "Invalid operator: " << expr[i] << endl;
-                return -1;
-            }
+                /* If there is "x" in between numbers and operator,
+                switch numbers position in stack */
+                case 'x': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(operand2);
+                    st.push(operand1);
+                    break;
+                }
+
+                case '+': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(operand1 + operand2);
+                    break;
+                }
+                case '-': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(operand1 - operand2);
+                    break;
+                }
+                case '*': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(operand1 * operand2);
+                    break;
+                }
+                case '/': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(operand1 / operand2);
+                    break;
+                }
+                case '%': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(operand1 % operand2);
+                    break;
+                }
+                //exponentiation
+                case '^': {
+                    int operand2 = st.top();
+                    st.pop();
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(pow(operand1, operand2));
+                    break;
+                }
+                //square root
+                case 'v': {
+                    int operand1 = st.top();
+                    st.pop();
+                    st.push(sqrt(operand1));
+                    break;
+                }
+            
+                default: {
+                    cerr << "Invalid operator: " << expr[i] << endl;
+                    return -1;
+                }
             }
         }
     }
     return st.top();
 }
 
-// Main function, ask user for expression
-int main() {
-    string expr;
-    cout << "Enter postfix expression: ";
-    getline(cin, expr);
+int prompt() {
+    string numbers, opr;
+    cout << "Enter operator (+ - * / % ^ v): ";
+    getline(cin, opr);
+    cout << "Enter two numbers you want to calculate: ";
+    getline(cin, numbers);
+    string expr = numbers + " " + opr;
+
     int result = postfix_calc(expr);
     cout << "Result: " << result << endl;
     return 0;
+}
+
+// Main function, ask user for expression
+int main(int argc, char* argv[]) {
+
+    //if user runs program with "-p" run it in prompt mode
+    if (argc == 2 && string(argv[1]) == "-p") {
+        prompt();
+    }
+    else{
+        string expr;
+        cout << "Enter postfix expression: ";
+        getline(cin, expr);
+        int result = postfix_calc(expr);
+        cout << "Result: " << result << endl;
+        return 0;
+    }
 }
